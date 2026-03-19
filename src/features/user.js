67c4259c3905e7e1aed0c830ae8d26a883d30b66
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-19 19:34:48 +0200

    feat: handling the state of the user

---
+++ src/features/user.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const userSlice = createSlice({
+    name:"user",
+    initialState: { value: { name:"didier",age:0,email:"didier@gmail.com" } },
+    reducers:{
+        login: (state,action)=> {
+            state.value = action.payload;
+        },
+
+        logout: (state,action) => {
+            state.value = { name:"",age:0,email:"" };
+        }
+
+    }
+});
+
+
+export  const {login,logout} = userSlice.actions;
+export default userSlice.reducer;
