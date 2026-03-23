ebfce602fa1cce3f46de9c63652825259503032a
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-23 10:33:58 +0200

    feat:rendering all components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Profile from './components/Profile'
 import Login from './components/Login'
 import ChangeColor from './components/ChangeColor'
+import Card from './components/Card'
+import CardProfile from './components/CardProfile'
+import ChangeCardData from './components/ChangeCardData'
 
 function App() {
   return (
@@ -9,6 +12,19 @@ function App() {
       <Profile />
       <Login />
       <ChangeColor/>
+
+      <h2>Card Component</h2>
+      <div className='card-container'>
+        <Card>
+          <div className='card-content'>
+            <div className='handle-flex-row'>
+              <CardProfile />
+              <ChangeCardData/>
+            </div>
+          </div>
+        </Card>
+      </div>
+      
     </div>
   )
 }
