01d5f084f4f90e60bd314791dca484c14d43e5f6
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-23 11:44:38 +0200

    feat: adding ingredients on the list

--- src/components/AddIngredients.jsx
+++ src/components/AddIngredients.jsx
@@ -1,18 +1,35 @@
-import { addIngredient } from "../features/ingredient"
-import { useDispatch } from "react-redux"
+import { addIngredient } from "../features/ingredient";
+import { useDispatch } from "react-redux";
 import { useState } from "react";
+
 function AddIngredients() {
   const dispatch = useDispatch();
-  const [formData ,setFormData] = useState("");
+  const [formData, setFormData] = useState("");
 
   return (
-    <div>
-        <h2>Add Ingredient used </h2>
-        <input type="text" placeholder="eg: coconut"  onChange={(event)=> {setFormData(event.target.value)}}/>
-        <button onClick={()=>{dispatch(addIngredient(formData))}}>Add Ingredient</button>
-
+    <div className="card-container">
+      <h2>Ingredient List</h2>
+      <div className="handle-flex-row">
+        <input 
+          type="text" 
+          placeholder="eg: coconut" 
+          value={formData}
+          onChange={(event) => setFormData(event.target.value)}
+        />
+        <button 
+          className="add-button" // You can add a button style in CSS later
+          onClick={() => {
+            if(formData) {
+              dispatch(addIngredient(formData));
+              setFormData(""); // Clear input after adding
+            }
+          }}
+        >
+          Add Ingredient
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddIngredients
+export default AddIngredients;
