748236a0b613e38ac0108cab230b9d8a672ed5d5
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-25 21:40:00 +0200

    feat: adding some dependencies

---
+++ src/components/HistoryNavigation.jsx
@@ -0,0 +1,21 @@
+// import { useHistory } from 'react-router-dom';
+
+// export default function MyComponent() {
+//     const history = useHistory();
+
+//     function handleClick() {
+//         // Programmatically navigate to a new route
+//         history.push('/new-route');
+//     }
+//     <button onClick={handleClick}>click</button>
+// }
+
+import React from 'react'
+
+function HistoryNavigation() {
+  return (
+    <div>HistoryNavigation</div>
+  )
+}
+
+export default HistoryNavigation
