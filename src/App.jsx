68cf5e7161b166fdef35d4c9042be8961b82a2e9
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-25 21:38:33 +0200

    feat: rendering using browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+ import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 import Profile from './components/Profile'
 import Login from './components/Login'
 import ChangeColor from './components/ChangeColor'
@@ -9,34 +11,68 @@ import AddIngredients from './components/AddIngredients'
 import DisplayIngredients from './components/DisplayIngredients'
 import MyComponent from './components/HistoryNavigation'
 import UncontrolledForm from './components/UnControlledForm'
-import Products from './components/Products'
+import Products from './prooductsFilter/Products'
+import Shopping from './shoppingCart/Shopping'
+
 function App() {
   return (
-    <div>
-      {/* <Profile />
-      <Login />
-      <ChangeColor/> */}
+    <BrowserRouter>
 
-      {/* <h2>Card Component</h2>
-      <div className='card-container'>
-        <Card>
-          <div className='card-content'>
-            <div className='handle-flex-row'>
-              <CardProfile />
-              <ChangeCardData/>
+      {/* 🔗 Simple Navigation */}
+      <nav style={{ display: 'flex', gap: '50px', marginBottom: '20px' ,marginLeft:'20px' }}>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/color">Change Color</Link>
+        <Link to="/card">Card</Link>
+        <Link to="/ingredients">Ingredients</Link>
+        <Link to="/form">Form</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/shopping">Shopping</Link>
+      </nav>
+
+      {/* 🧭 Routes */}
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/color" element={<ChangeColor />} />
+
+        <Route 
+          path="/card" 
+          element={
+            <div className='card-container'>
+              <Card>
+                <div className='card-content'>
+                  <div className='handle-flex-row'>
+                    <CardProfile />
+                    <ChangeCardData />
+                  </div>
+                </div>
+              </Card>
             </div>
-          </div>
-        </Card>
-      </div> */}
-      {/* <AddIngredients/>
-      <DisplayIngredients/>
-      <MyComponent/>
-      <UncontrolledForm/> */}
+          } 
+        />
 
-      <Products/>
-   
-      
-    </div>
+        <Route 
+          path="/ingredients" 
+          element={
+            <>
+              <AddIngredients />
+              <DisplayIngredients />
+            </>
+          } 
+        />
+
+        <Route path="/history" element={<MyComponent />} />
+        <Route path="/form" element={<UncontrolledForm />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/shopping" element={<Shopping />} />
+
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
